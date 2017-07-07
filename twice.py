500cbb50deeb86a1aeadad9413ba895fdefb8b1a
zhsir123 <zhsir123@gmail.com>
2017-07-07 22:43:08 +0900

    init + make feed and label

---
+++ twice.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
--- twice_input.py
+++ twice_input.py
@@ -1,16 +1,25 @@
 import numpy as np
 import os
 from PIL import Image
-from tflearn.data_utils import shuffle
+from sklearn.utils import shuffle
 
-dataList = list()
-
-directories = os.listdir('data')
-for directory in directories:
-    dataList.append([np.array(Image.open('data/' + directory + '/' + file)).flatten() for file in os.listdir('data/' + directory)])
-
-print(dataList)
+IMAGE_SIZE = 3072
+LABEL_SIZE = 844
 
 
+def get_feed_data():
+    directories = os.listdir('data')
+    files = [file for directory in directories for file in os.listdir('data/' + directory)]
+    data_len = len(files)
 
+    img_matrix = np.array([np.array(Image.open('data/' + directory + '/' + file)).flatten() for directory in directories
+                          for file in os.listdir('data/' + directory)], 'f')
+    label = np.ones((data_len, ), dtype=int)
 
+    # 0 : jeongyeon, 1 : momo, 2: nayeon
+    label[:248] = 0
+    label[248: 547] = 1
+    label[547: 844] = 2
+
+    data, label = shuffle(img_matrix, label, random_state=4)
+    return data, label
